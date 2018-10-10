8fdc75bfc0116770d8b2efb541e4f91eef524a95
Alexander Schulz <code@schulzalex.de>
2018-10-10 19:23:52 +0200

    temporarly restrict license (to match real XCP-ng host features until it can report correctly)

--- XenModel/XenAPI-Extensions/Host.cs
+++ XenModel/XenAPI-Extensions/Host.cs
@@ -453,9 +453,6 @@ namespace XenAPI
 
         public static bool RestrictCorosync(Host h)
         {
-            // GFS2 and Clustering is not (yet) supported on XCP-ng
-            return true;
-
             return BoolKeyPreferTrue(h.license_params, "restrict_corosync");
         }
 
