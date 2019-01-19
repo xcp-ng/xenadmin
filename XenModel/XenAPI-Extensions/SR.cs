80ac397842f0ebbb93a276e8bf34919b5020d754
cocoon <cocoon@wolke7.net>
2019-01-19 23:22:18 +0100

    add ext4 and xfs as SR type

--- XenModel/XenAPI-Extensions/SR.cs
+++ XenModel/XenAPI-Extensions/SR.cs
@@ -49,13 +49,13 @@ namespace XenAPI
         /// </summary>
         public enum SRTypes
         {
-            local, ext, lvmoiscsi, iso, nfs, lvm, netapp, udev, lvmofc,
+            local, ext, ext4, lvmoiscsi, iso, nfs, lvm, netapp, udev, lvmofc,
             lvmohba, egenera, egeneracd, dummy, unknown, equal, cslg, shm,
             iscsi,
             ebs, rawhba,
             smb, lvmofcoe, gfs2,
             nutanix, nutanixiso, 
-            tmpfs
+            tmpfs, xfs
         }
 
         public const long DISK_MAX_SIZE = 2 * Util.BINARY_TERA;
@@ -245,7 +245,9 @@ namespace XenAPI
                 || type == SRTypes.cslg
                 || type == SRTypes.smb
                 || type == SRTypes.lvmofcoe
-                || type == SRTypes.gfs2;
+                || type == SRTypes.gfs2
+                || type == SRTypes.ext4
+                || type == SRTypes.xfs;
         }
 
         public bool IsLocalSR()
@@ -256,7 +258,8 @@ namespace XenAPI
                    || typ == SRTypes.lvm
                    || typ == SRTypes.udev
                    || typ == SRTypes.egeneracd
-                   || typ == SRTypes.dummy;
+                   || typ == SRTypes.dummy
+                   || typ == SRTypes.ext4;
         }
 
         public bool ShowForgetWarning()
