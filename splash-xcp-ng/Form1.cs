edab3061393cfb4230bf3e7f00b86357e0b983fd
Alexander Schulz <code@schulzalex.de>
2019-05-11 17:32:10 +0200

    removing old splash, adding new XCP-ng splash from Cocoon (Thanks!)
    (cherry picked from commit 445d838c71d5cac4fb3908afcc4a565dfaf83257)

---
+++ splash-xcp-ng/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace splash_xcp_ng
+{
+    public partial class Form1 : Form
+    {
+        private System.IO.Stream stream;
+        private System.Reflection.Assembly assembly;
+        const string exe = "XenCenterMain.exe";
+        Version AssemblyVersion = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
+        string ProductVersion = System.Windows.Forms.Application.ProductVersion; 
+
+        private BackgroundWorker bworker = new BackgroundWorker();
+        private ProcessStartInfo startInfo = new ProcessStartInfo();
+        private Process proc;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Style();
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            Application.DoEvents();
+            Launch();
+        }
+
+        private void Style()
+        {
+            labelVersion.Text = ProductVersion + " (" + AssemblyVersion + ")";
+
+            Image bitmap;
+            assembly = System.Reflection.Assembly.LoadFrom(Application.ExecutablePath);
+            stream = assembly.GetManifestResourceStream("splash_xcp_ng.Resources.splash.bmp");
+            bitmap = Image.FromStream(stream);
+
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.DoubleBuffer, true);
+            this.BackgroundImage = bitmap;
+            this.BackgroundImageLayout = ImageLayout.Stretch;
+            this.Width = bitmap.Width;
+            this.Height = bitmap.Height;
+            this.CenterToScreen();
+        }
+
+        private void Launch()
+        {
+            if (!File.Exists(exe))
+            {
+                MessageBox.Show("[ERROR] Application not found: " + exe);
+                Exit();
+                return;
+            }
+
+
+            startInfo.CreateNoWindow = false;
+            startInfo.UseShellExecute = false;
+            startInfo.FileName = exe;
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            startInfo.Arguments = String.Empty;
+
+            bworker.WorkerSupportsCancellation = true;
+            bworker.DoWork += new DoWorkEventHandler(Start);
+            bworker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(bworkerCompleted);
+            bworker.RunWorkerAsync();
+
+            //FixMe: Add some safty counter to exit if something goes wrong
+            while (proc == null || string.IsNullOrEmpty(proc.MainWindowTitle))
+            {
+                System.Threading.Thread.Sleep(100);
+                if(proc != null) proc.Refresh();
+            }
+
+            Exit();
+
+        }
+
+        private void Start(object sender, DoWorkEventArgs e)
+        {
+                proc = Process.Start(startInfo);
+        }
+
+        private void bworkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+        }
+
+        private void Exit()
+        {
+            if (bworker != null && bworker.WorkerSupportsCancellation) bworker.CancelAsync();
+            System.Environment.Exit(1);
+        }
+
+
+    }
+}
---
+++ splash-xcp-ng/SplashForm.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace splash_xcp_ng
+{
+    partial class SplashForm
+    {
+        private System.ComponentModel.IContainer components = null;
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.labelVersion = new System.Windows.Forms.Label();
+            this.panelMain = new System.Windows.Forms.Panel();
+            this.panelMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // labelVersion
+            // 
+            this.labelVersion.AutoSize = true;
+            this.labelVersion.BackColor = System.Drawing.Color.Transparent;
+            this.labelVersion.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.labelVersion.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelVersion.Location = new System.Drawing.Point(10, 531);
+            this.labelVersion.Margin = new System.Windows.Forms.Padding(0);
+            this.labelVersion.Name = "labelVersion";
+            this.labelVersion.Size = new System.Drawing.Size(0, 13);
+            this.labelVersion.TabIndex = 0;
+            // 
+            // panelMain
+            // 
+            this.panelMain.AutoSize = true;
+            this.panelMain.BackColor = System.Drawing.Color.Transparent;
+            this.panelMain.Controls.Add(this.labelVersion);
+            this.panelMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelMain.Location = new System.Drawing.Point(0, 0);
+            this.panelMain.Margin = new System.Windows.Forms.Padding(0);
+            this.panelMain.Name = "panelMain";
+            this.panelMain.Padding = new System.Windows.Forms.Padding(10, 0, 0, 10);
+            this.panelMain.Size = new System.Drawing.Size(800, 554);
+            this.panelMain.TabIndex = 1;
+            // 
+            // SplashForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 554);
+            this.Controls.Add(this.panelMain);
+            this.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "SplashForm";
+            this.Text = "XCP-ng Center Launcher";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.panelMain.ResumeLayout(false);
+            this.panelMain.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.Label labelVersion;
+        private System.Windows.Forms.Panel panelMain;
+    }
+}
+
